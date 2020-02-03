93d3c933b7db72321ff9c79689246d3c07acf5c2
neelamkoshiya <neelamkoshiya@gmail.com>
2020-02-03 06:46:49 -0800

    Add files via upload

--- Images/testrecommendation.py
+++ Images/testrecommendation.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
     ItemId = event["itemId"]
     UserId = event["userId"]
     response = personalizeRt.get_recommendations(
-        campaignArn = "arn:aws:personalize:us-west-2:340280328827:campaign/retailcampaign",
+        campaignArn = "arn:aws:personalize:region:accountid:campaign/revisedcampaign",
         itemId =ItemId,
         userId =UserId,
         numResults=6
@@ -14,13 +14,16 @@ def lambda_handler(event, context):
     for item in response['itemList']:
         if item['itemId'] ==ItemId:
             print('input item')
-            print(item['itemId'])
             continue
         else:
             count += 1
             if count < 6:
                 print('new item')
-                print(item['itemId'])
                 newlist.append(item)
             else:
                 continue
+
+
+
+
+    return newlist
